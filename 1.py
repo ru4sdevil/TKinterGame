8c79d2cb5542be309138742c8f97d92b8587c60b
ru4sdevil <ru4sdevil@gmail.com>
2019-08-19 15:18:06 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,13 +4,28 @@ import random
 def random_pos(top):
     return random.randint(1, top-1)*step
 
+class Player:
+
+    def __init__(this, color):
+        this.x = random_pos(N_X)
+        this.y = random_pos(N_Y)
+        this.color = color
+
+    def draw(this):
+        canvas.create_oval((this.x, this.y), (this.x+step, this.y+step), fill = this.color)
+        
+    def random_pos(top):
+        return random.randint(1, top-1)*step
+
 master = tk.Tk()
 step = 60
 N_X = 10
 N_Y = 10
 canvas = tk.Canvas(master, bg='blue', height = step*N_X, width = step*N_Y)
 
-
-
-player_pos = (random_pos(N_X),  random_pos(N_Y))
-exit_pos = (random_pos(N_X), random_pos(N_Y))
+player = Player('green')
+player.draw()
+exit_q = Player('yellow')
+exit_q.draw()
+canvas.pack()
+master.mainloop()
