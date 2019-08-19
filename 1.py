6b7a469bcc3c8eb81ef93b2cd1f8f55cd350c352
ru4sdevil <ru4sdevil@gmail.com>
2019-08-19 14:52:44 +0300

    random_pos

--- 1.py
+++ 1.py
@@ -1,1 +1,16 @@
-print('Hello world!')
+import tkinter as tk
+import random
+
+def random_pos(top):
+    return random.randint(1, top-1)*step
+
+master = tk.Tk()
+step = 60
+N_X = 10
+N_Y = 10
+canvas = tk.Canvas(master, bg='blue', height = step*N_X, width = step*N_Y)
+
+
+
+player_pos = (random_pos(N_X),  random_pos(N_Y))
+exit_pos = (random_pos(N_X), random_pos(N_Y))
