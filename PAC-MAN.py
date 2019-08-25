eb21b8fa4665b547aee7f933ac4ad4f6fdcf081f
ru4sdevil <ru4sdevil@gmail.com>
2019-08-25 07:48:44 +0300

    Walls NSEW

--- PAC-MAN.py
+++ PAC-MAN.py
@@ -4,21 +4,43 @@ import tkinter as tk
 step = 30
 N_X = 33
 N_Y = 11
+
+
 master = tk.Tk()
 canvas = tk.Canvas(master, bg='black', height = step*N_Y, width = step*N_X)
 
-class Wall():
-    def __init__(this, x, y, color = 'blue'):
-        this.x = x
-        this.y = y
+class Wall:
+
+    def __init__(this, x_1 = False, y_1 = False, x_2 = False, y_2 = False, color = 'blue'):
+        this.x_1, this.y_1 = x_1, y_1
+        this.x_2, this.y_2 = x_2, y_2
         this.color = color
-        this.outline = color
         this.draw()
 
     def draw(this):
-        this.body = canvas.create_rectangle((this.x, this.y), fill = this.color, outline = this.outline)
+       this.wall = canvas.create_rectangle((this.x_1, this.y_1), (this.x_2, this.y_2), fill = this.color, outline = this.color) 
+
+class N_wall(Wall):
+    def __init__(this, x_1 = 0, y_1 = 0, x_2 = step * N_X, y_2 = step):
+        super().__init__(x_1, y_1, x_2, y_2)
+
+class S_wall(Wall):
+    def __init__(this, x_1 = 0, y_1 = step * (N_Y - 1), x_2 = step * N_X, y_2 = step * N_Y):
+        super().__init__(x_1, y_1, x_2, y_2)
+
+class E_wall(Wall):
+    def __init__(this, x_1 = 0, y_1 = 0, x_2 = step * 2, y_2 = step * (N_Y - 2)):
+        super().__init__(x_1, y_1, x_2, y_2)
+
+class W_wall(Wall):
+    def __init__(this, x_1 = step * (N_X - 2), y_1 = step * 2, x_2 = step * N_X, y_2 = step * N_Y):
+        super().__init__(x_1, y_1, x_2, y_2)
         
-wall = Wall()
+n_wall = N_wall()
+s_wall = S_wall()
+e_wall = E_wall()
+w_wall = W_wall()
 
 canvas.pack()
 master.mainloop()
+
